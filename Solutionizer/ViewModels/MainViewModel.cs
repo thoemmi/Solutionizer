673371f6792f77f4d6013e728f7bb185dfd00ab3
thoemmi <info@thomasfreudenberg.com>
2011-11-10 12:21:34 +0100

    moved project selection to its own view/viewmodel

--- Solutionizer/ViewModels/MainViewModel.cs
+++ Solutionizer/ViewModels/MainViewModel.cs
@@ -1,28 +1,25 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Windows;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Command;
 using Ookii.Dialogs.Wpf;
-using Solutionizer.Scanner;
 
 namespace Solutionizer.ViewModels {
     public class MainViewModel : ViewModelBase {
         private bool _isBusy;
         private string _busyMessage;
-        private string _rootPath = @"d:\dev\xtplus\main\main";
+        private SolutionViewModel _solution = new SolutionViewModel();
+        private readonly ProjectShelfViewModel _projectShelfViewModel;
         private readonly ICommand _onLoadedCommand;
         private readonly ICommand _selectRootPathCommand;
-        private IList<DirectoryNode> _rootNodes;
-        private SolutionViewModel _solution = new SolutionViewModel();
         private readonly ICommand _launchCommand;
         private readonly ICommand _saveCommand;
 
         public MainViewModel() {
-            _onLoadedCommand = new RelayCommand(RefreshFileTree);
+            _projectShelfViewModel = new ProjectShelfViewModel(this);
+            _onLoadedCommand = new RelayCommand(OnLoaded);
             _selectRootPathCommand = new RelayCommand(OnSelectRootPath);
             _launchCommand = new RelayCommand(OnLaunch, () => _solution.Projects.Any());
             _saveCommand = new RelayCommand(OnSave, () => _solution.Projects.Any());
@@ -30,29 +27,15 @@ namespace Solutionizer.ViewModels {
 
         private void OnSelectRootPath() {
             var dlg = new VistaFolderBrowserDialog {
-                SelectedPath = RootPath
+                SelectedPath = _projectShelfViewModel.RootPath
             };
             if (dlg.ShowDialog(Application.Current.MainWindow) == true) {
-                RootPath = dlg.SelectedPath;
-                RefreshFileTree();
+                _projectShelfViewModel.RootPath = dlg.SelectedPath;
             }
         }
 
-        private void RefreshFileTree() {
-            IsBusy = true;
-
-            var worker = new BackgroundWorker();
-            worker.DoWork += (o, ea) => {
-                var scanner = new ProjectScanner();
-                ea.Result = new[] {
-                    scanner.Scan(RootPath)
-                };
-            };
-            worker.RunWorkerCompleted += (o, ea) => {
-                IsBusy = false;
-                RootNodes = (IList<DirectoryNode>) ea.Result;
-            };
-            worker.RunWorkerAsync();
+        private void OnLoaded() {
+            _projectShelfViewModel.RootPath = @"d:\dev\xtplus\main\main";
         }
 
         private void OnLaunch() {
@@ -70,7 +53,7 @@ namespace Solutionizer.ViewModels {
                 // TODO save solution
             }
         }
-
+        
         public ICommand OnLoadedCommand {
             get { return _onLoadedCommand; }
         }
@@ -107,24 +90,8 @@ namespace Solutionizer.ViewModels {
             }
         }
 
-        public string RootPath {
-            get { return _rootPath; }
-            set {
-                if (_rootPath != value) {
-                    _rootPath = value;
-                    RaisePropertyChanged(() => RootPath);
-                }
-            }
-        }
-
-        public IList<DirectoryNode> RootNodes {
-            get { return _rootNodes; }
-            set {
-                if (_rootNodes != value) {
-                    _rootNodes = value;
-                    RaisePropertyChanged(() => RootNodes);
-                }
-            }
+        public ProjectShelfViewModel ProjectShelf {
+            get { return _projectShelfViewModel; }
         }
 
         public SolutionViewModel Solution {
---
+++ Solutionizer/ViewModels/ProjectShelfViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Solutionizer.Scanner;
+
+namespace Solutionizer.ViewModels {
+    public class ProjectShelfViewModel : ViewModelBase {
+        private readonly MainViewModel _mainViewModel;
+        private string _rootPath;
+        private IList<DirectoryNode> _rootNodes;
+
+        public ProjectShelfViewModel(MainViewModel mainViewModel) {
+            _mainViewModel = mainViewModel;
+        }
+
+        private void RefreshFileTree() {
+            _mainViewModel.IsBusy = true;
+
+            var worker = new BackgroundWorker();
+            worker.DoWork += (o, ea) => {
+                var scanner = new ProjectScanner();
+                ea.Result = new[] {
+                    scanner.Scan(RootPath)
+                };
+            };
+            worker.RunWorkerCompleted += (o, ea) => {
+                _mainViewModel.IsBusy = false;
+                RootNodes = (IList<DirectoryNode>) ea.Result;
+            };
+            worker.RunWorkerAsync();
+        }
+
+        public IList<DirectoryNode> RootNodes {
+            get { return _rootNodes; }
+            set {
+                if (_rootNodes != value) {
+                    _rootNodes = value;
+                    RaisePropertyChanged(() => RootNodes);
+                }
+            }
+        }
+
+        public string RootPath {
+            get { return _rootPath; }
+            set {
+                if (_rootPath != value) {
+                    _rootPath = value;
+                    RaisePropertyChanged(() => RootPath);
+                    RefreshFileTree();
+                }
+            }
+        }
+    }
+}
