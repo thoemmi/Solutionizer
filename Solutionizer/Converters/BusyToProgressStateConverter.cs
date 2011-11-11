cf62c94d5c9cfc6cd38a4febe5b1a4726460811c
thoemmi <info@thomasfreudenberg.com>
2011-11-11 16:15:00 +0100

    Busy is now reflected in taskbaritem

---
+++ Solutionizer/Converters/BusyToProgressStateConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Shell;
+
+namespace Solutionizer.Converters {
+    public class BusyToProgressStateConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return ((bool)value) ? TaskbarItemProgressState.Indeterminate : TaskbarItemProgressState.None;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
