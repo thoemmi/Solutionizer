d97751bcd73de67a3e7419433d37a006eaf4a754
thoemmi <info@thomasfreudenberg.com>
2011-11-11 16:07:25 +0100

    removed MainWindow; MainView is a window now

--- Solutionizer/Views/MainView.xaml.cs
+++ Solutionizer/Views/MainView.xaml.cs
@@ -1,11 +1,11 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
 using Solutionizer.ViewModels;
 
 namespace Solutionizer.Views {
     /// <summary>
     /// Interaction logic for MainView.xaml
     /// </summary>
-    public partial class MainView : UserControl {
+    public partial class MainView : Window {
         public MainView() {
             InitializeComponent();
             DataContext = new MainViewModel();
