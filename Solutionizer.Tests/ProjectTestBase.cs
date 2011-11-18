ffcc53a09c88442a7ae498cf26fa29d3d827f3b7
thoemmi <info@thomasfreudenberg.com>
2011-11-18 15:35:23 +0100

    more tests for ProjectScanner

--- Solutionizer.Tests/ProjectTestBase.cs
+++ Solutionizer.Tests/ProjectTestBase.cs
@@ -17,17 +17,7 @@ namespace Solutionizer.Tests {
             Directory.Delete(_testDataPath, true);
         }
 
-        protected void CreateProjects() {
-            var p1 = Path.Combine(_testDataPath, "Project1");
-            Directory.CreateDirectory(p1);
-            CopyTestDataToPath("CsTestProject1.csproj", p1);
-
-            var p2 = Path.Combine(_testDataPath, "Project2");
-            Directory.CreateDirectory(p2);
-            CopyTestDataToPath("CsTestProject2.csproj", p2);
-        }
-
-        private static void CopyTestDataToPath(string resource, string path) {
+        protected void CopyTestDataToPath(string resource, string path) {
             Directory.CreateDirectory(path);
             var stream = typeof(ProjectTestBase).Assembly.GetManifestResourceStream("Solutionizer.Tests.TestData." + resource);
             using (var input = new StreamReader(stream))
--- Solutionizer.Tests/ScannerTest.cs
+++ Solutionizer.Tests/ScannerTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.IO;
+using NUnit.Framework;
 using Solutionizer.Scanner;
 
 namespace Solutionizer.Tests {
@@ -11,6 +12,44 @@ namespace Solutionizer.Tests {
             Assert.AreEqual(_testDataPath, root.Path);
             Assert.AreEqual(_testDataPath, root.Name);
             Assert.IsEmpty(root.Subdirectories);
+            Assert.IsEmpty(root.Files);
+        }
+
+        [Test]
+        public void EmptySubdirectoriesAreOmitted() {
+            Directory.CreateDirectory(Path.Combine(_testDataPath, "YouShouldNotSeeMe"));
+
+            var scanner = new ProjectScanner();
+            var root = scanner.Scan(_testDataPath);
+            Assert.AreEqual(_testDataPath, root.Path);
+            Assert.AreEqual(_testDataPath, root.Name);
+            Assert.IsEmpty(root.Subdirectories);
+            Assert.IsEmpty(root.Files);
+        }
+
+        [Test]
+        public void CanScanTwoProjectsInRoot() {
+            CopyTestDataToPath("CsTestProject1.csproj", _testDataPath);
+            CopyTestDataToPath("CsTestProject2.csproj", _testDataPath);
+
+            var scanner = new ProjectScanner();
+            var root = scanner.Scan(_testDataPath);
+            Assert.AreEqual(_testDataPath, root.Path);
+            Assert.AreEqual(_testDataPath, root.Name);
+            Assert.IsEmpty(root.Subdirectories);
+            Assert.AreEqual(2, root.Files.Count);
+        }
+
+        [Test]
+        public void CanScanTwoProjectsInSubdirectories() {
+            CopyTestDataToPath("CsTestProject1.csproj", Path.Combine(_testDataPath, "p1"));
+            CopyTestDataToPath("CsTestProject2.csproj", Path.Combine(_testDataPath, "p2"));
+
+            var scanner = new ProjectScanner();
+            var root = scanner.Scan(_testDataPath);
+            Assert.AreEqual(_testDataPath, root.Path);
+            Assert.AreEqual(_testDataPath, root.Name);
+            Assert.AreEqual(2, root.Subdirectories.Count);
             Assert.IsEmpty(root.Files);
         }
     }
