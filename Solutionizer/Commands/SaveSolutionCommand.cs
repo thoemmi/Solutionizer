fc560fea4f13ce58c0dbdd67212f40c044edd6f6
thoemmi <info@thomasfreudenberg.com>
2011-11-21 10:20:40 +0100

    Moved GetRelativePath to separate class

--- Solutionizer/Commands/SaveSolutionCommand.cs
+++ Solutionizer/Commands/SaveSolutionCommand.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.IO;
-using System.Runtime.InteropServices;
-using System.Text;
 using Solutionizer.Extensions;
+using Solutionizer.Helper;
 using Solutionizer.ViewModels;
 
 namespace Solutionizer.Commands {
@@ -24,7 +22,7 @@ namespace Solutionizer.Commands {
 
                 foreach (var project in projects) {
                     streamWriter.WriteLine("Project(\"{0}\") = \"{1}\", \"{2}\", \"{3}\"", "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}",
-                                           project.Name, GetRelativePath(_solutionFileName, project.Project.Filepath),
+                                           project.Name, FileSystem.GetRelativePath(_solutionFileName, project.Project.Filepath),
                                            project.Guid.ToString("B").ToUpperInvariant());
                     streamWriter.WriteLine("EndProject");
                 }
@@ -72,42 +70,5 @@ namespace Solutionizer.Commands {
             }
             streamWriter.WriteLine("\tEndGlobalSection");
         }
-
-        public static string GetRelativePath(string fromPath, string toPath) {
-            var fromAttr = GetPathAttribute(fromPath);
-            var toAttr = GetPathAttribute(toPath);
-
-            var path = new StringBuilder(260); // MAX_PATH
-            if (PathRelativePathTo(path, fromPath, fromAttr, toPath, toAttr) == 0) {
-                throw new ArgumentException("Paths must have a common prefix");
-            }
-            var relativePath = path.ToString();
-            if (relativePath.StartsWith(@".\")) {
-                return relativePath.Substring(2);
-            } else {
-                return relativePath;
-            }
-        }
-
-        private static int GetPathAttribute(string path) {
-            var di = new DirectoryInfo(path);
-            if (di.Exists) {
-                return FILE_ATTRIBUTE_DIRECTORY;
-            }
-
-            var fi = new FileInfo(path);
-            if (fi.Exists) {
-                return FILE_ATTRIBUTE_NORMAL;
-            }
-
-            throw new FileNotFoundException();
-        }
-
-        private const int FILE_ATTRIBUTE_DIRECTORY = 0x10;
-        private const int FILE_ATTRIBUTE_NORMAL = 0x80;
-
-        [DllImport("shlwapi.dll", SetLastError = true)]
-        private static extern int PathRelativePathTo(StringBuilder pszPath,
-                                                     string pszFrom, int dwAttrFrom, string pszTo, int dwAttrTo);
     }
 }
---
+++ Solutionizer/Helper/FileSystem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Solutionizer.Helper {
+    public static class FileSystem {
+        public static string GetRelativePath(string fromPath, string toPath) {
+            var fromAttr = GetPathAttribute(fromPath);
+            var toAttr = GetPathAttribute(toPath);
+
+            var path = new StringBuilder(260); // MAX_PATH
+            if (PathRelativePathTo(path, fromPath, fromAttr, toPath, toAttr) == 0) {
+                throw new ArgumentException("Paths must have a common prefix");
+            }
+            var relativePath = path.ToString();
+            return relativePath.StartsWith(@".\") ? relativePath.Substring(2) : relativePath;
+        }
+
+        private static int GetPathAttribute(string path) {
+            var di = new DirectoryInfo(path);
+            if (di.Exists) {
+                return FILE_ATTRIBUTE_DIRECTORY;
+            }
+
+            var fi = new FileInfo(path);
+            if (fi.Exists) {
+                return FILE_ATTRIBUTE_NORMAL;
+            }
+
+            throw new FileNotFoundException();
+        }
+
+        private const int FILE_ATTRIBUTE_DIRECTORY = 0x10;
+        private const int FILE_ATTRIBUTE_NORMAL = 0x80;
+
+        [DllImport("shlwapi.dll", SetLastError = true)]
+        private static extern int PathRelativePathTo(StringBuilder pszPath,
+                                                     string pszFrom, int dwAttrFrom, string pszTo, int dwAttrTo);
+    }
+}
