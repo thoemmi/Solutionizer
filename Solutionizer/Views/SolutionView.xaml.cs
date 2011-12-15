c2ec20004d555156c0caa8da3f95e5f01f32cfa0
thoemmi <info@thomasfreudenberg.com>
2011-12-15 12:12:38 +0100

    better selection handling when removing a solution item

--- Solutionizer/Views/SolutionView.xaml.cs
+++ Solutionizer/Views/SolutionView.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using Solutionizer.Extensions;
 using Solutionizer.Models;
+using Solutionizer.VisualStudio;
 
 namespace Solutionizer.Views {
     /// <summary>
@@ -13,9 +16,30 @@ namespace Solutionizer.Views {
 
         private void TreeView_KeyDown(object sender, KeyEventArgs e) {
             if (e.Key == Key.Delete) {
-                //var si = _tree.SelectedItem;
+                var visual = e.OriginalSource as Visual;
+                var parentTreeViewItem = visual.TryFindParent<TreeViewItem>();
+
+                var selectedItem = _tree.SelectedItem as SolutionItem;
+                if (selectedItem == null) {
+                    return;
+                }
+
+                var index = parentTreeViewItem.Items.IndexOf(selectedItem);
+
                 var viewModel = (SolutionViewModel) DataContext;
-                viewModel.RemoveSolutionItemCommand.Execute(_tree.SelectedItem);
+                viewModel.RemoveSolutionItemCommand.Execute(selectedItem);
+
+                if (index >= 0) {
+                    if (index >= parentTreeViewItem.Items.Count) {
+                        index--;
+                    }
+                    if (index >= 0) {
+                        ((TreeViewItem) parentTreeViewItem.ItemContainerGenerator.ContainerFromItem(parentTreeViewItem.Items[index])).
+                            IsSelected = true;
+                    } else {
+                        parentTreeViewItem.IsSelected = true;
+                    }
+                }
             }
         }
     }
