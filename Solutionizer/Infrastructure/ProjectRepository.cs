f4f85442fdb3cbcda067c38f789f639b1d99506b
thoemmi <info@thomasfreudenberg.com>
2012-03-25 18:59:29 +0200

    refactoring

--- Solutionizer/Infrastructure/ProjectRepository.cs
+++ Solutionizer/Infrastructure/ProjectRepository.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Solutionizer.Helper;
 using Solutionizer.Models;
 
 namespace Solutionizer.Infrastructure {
@@ -19,17 +20,43 @@ namespace Solutionizer.Infrastructure {
         private readonly ConcurrentDictionary<string,Project> _projects = new ConcurrentDictionary<string, Project>(StringComparer.InvariantCultureIgnoreCase);
 
         public IEnumerable<Project> GetProjects(string rootPath) {
-            var projects = new List<Project>();
-            // enumerate all projects and yield them
-            foreach (var project in GetOrAddProjects(rootPath)) {
-                projects.Add(project);
-                yield return project;
-            }
+            var projects = GetOrAddProjects(rootPath).ToList();
+
+            PopulateLogicalPaths(rootPath, projects);
 
             // load project details asynchronously
             foreach (var p in projects) {
                 var project = p;
                 Task.Factory.StartNew(project.Load);
+            }
+            return projects;
+        }
+
+        private void PopulateLogicalPaths(string rootPath, List<Project> projects) {
+            var separators = new[] { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar };
+            var ps = projects.ToDictionary(
+                project => project,
+                project => FileSystem.GetRelativePath(rootPath, Path.GetDirectoryName(project.Filepath)).Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                );
+
+            var folder = new ProjectFolder(String.Empty);
+            Populate(folder, ps);
+        }
+
+        private void Populate(ProjectFolder folder, Dictionary<Project, string[]> projects) {
+            var directChildren = projects.Where(pair => pair.Value.Length == 0).Select(pair => pair.Key).ToList();
+            folder.Projects.AddRange(directChildren);
+            foreach (var directChild in directChildren) {
+                projects.Remove(directChild);
+            }
+
+            var subfolderNames = projects.GroupBy(pair => pair.Value[0]).ToList();
+            foreach (var subfolderName in subfolderNames) {
+                var subthing = new ProjectFolder(subfolderName.Key);
+                folder.Folders.Add(subthing);
+
+                var subprojects = subfolderName.ToDictionary(pair => pair.Key, pair => pair.Value.Skip(1).ToArray());
+                Populate(subthing, subprojects);
             }
         }
 
---
+++ Solutionizer/Models/ProjectFolder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Solutionizer.Models {
+    public class ProjectFolder {
+        private readonly string _name;
+        private readonly List<ProjectFolder> _folders = new List<ProjectFolder>();
+        private readonly List<Project> _projects = new List<Project>();
+
+        public ProjectFolder(string name) {
+            _name = name;
+        }
+
+        public string Name {
+            get { return _name; }
+        }
+
+        public List<ProjectFolder> Folders {
+            get { return _folders; }
+        }
+
+        public List<Project> Projects {
+            get { return _projects; }
+        }
+    }
+}
