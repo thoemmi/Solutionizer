32c5e3d7d81846db3eb4403edf1985c5d12eb1d8
thoemmi <info@thomasfreudenberg.com>
2012-04-05 15:35:45 +0200

    working on drag'n'drop

--- Solutionizer/Solution/SolutionViewModel.cs
+++ Solutionizer/Solution/SolutionViewModel.cs
@@ -1,5 +1,12 @@
-﻿namespace Solutionizer.Solution {
+﻿using System.Windows;
+
+namespace Solutionizer.Solution {
     public class SolutionViewModel {
-         
+        public void PreviewDragOver(DragEventArgs e) {
+            var x = e.Data.GetFormats();
+        }
+
+        public void PreviewDrop(DragEventArgs e) {
+        }
     }
 }
