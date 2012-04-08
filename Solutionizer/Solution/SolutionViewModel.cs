f509528f5462eb57196293fb65797f9a3685710a
unknown <tsfreude@HV-lap204.pharmatechnik.de>
2012-04-08 16:53:54 +0200

    drag'n'drop

--- Solutionizer/Solution/SolutionViewModel.cs
+++ Solutionizer/Solution/SolutionViewModel.cs
@@ -1,12 +1,25 @@
 ﻿using System.Windows;
+using System.Windows.Input;
+using Solutionizer.Infrastructure;
+using Solutionizer.ViewModels;
 
 namespace Solutionizer.Solution {
     public class SolutionViewModel {
-        public void PreviewDragOver(DragEventArgs e) {
-            var x = e.Data.GetFormats();
+        private readonly ICommand _dropCommand;
+
+        public SolutionViewModel() {
+            _dropCommand = new FixedRelayCommand<object>(OnDrop, obj => obj is ProjectViewModel);
         }
 
-        public void PreviewDrop(DragEventArgs e) {
+        private void OnDrop(object node)
+        {
+            var project = ((ProjectViewModel)node).Project;
+            project.Load();
+            //AddProject(project);
+        }
+
+        public ICommand DropCommand {
+            get { return _dropCommand; }
         }
     }
 }
