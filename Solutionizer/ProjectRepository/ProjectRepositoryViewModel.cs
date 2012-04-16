ef96f55f50a3c12c740483fd04b980b5dec1f8da
thoemmi <info@thomasfreudenberg.com>
2012-04-16 09:05:29 +0200

    bring back flat mode in project repository

--- Solutionizer/ProjectRepository/ProjectRepositoryViewModel.cs
+++ Solutionizer/ProjectRepository/ProjectRepositoryViewModel.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Caliburn.Micro;
 using Solutionizer.Models;
 using Solutionizer.ViewModels;
+using Solutionizer.Extensions;
 
 namespace Solutionizer.ProjectRepository {
     public class ProjectRepositoryViewModel : PropertyChangedBase {
@@ -43,11 +44,17 @@ namespace Solutionizer.ProjectRepository {
 
         private DirectoryViewModel CreateDirectoryViewModel(ProjectFolder projectFolder, DirectoryViewModel parent) {
             var viewModel = new DirectoryViewModel(projectFolder, parent);
-            foreach (var folder in projectFolder.Folders) {
-                viewModel.Directories.Add(CreateDirectoryViewModel(folder, viewModel));
-            }
-            foreach (var project in projectFolder.Projects) {
-                viewModel.Projects.Add(CreateProjectViewModel(project, viewModel));
+            if (Services.Settings.Instance.IsFlatMode) {
+                foreach (var project in new[]{projectFolder}.Flatten(f => f.Projects, f => f.Folders)) {
+                    viewModel.Projects.Add(CreateProjectViewModel(project, viewModel));
+                }
+            } else {
+                foreach (var folder in projectFolder.Folders) {
+                    viewModel.Directories.Add(CreateDirectoryViewModel(folder, viewModel));
+                }
+                foreach (var project in projectFolder.Projects) {
+                    viewModel.Projects.Add(CreateProjectViewModel(project, viewModel));
+                }
             }
             return viewModel;
         }
