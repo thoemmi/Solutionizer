5f791cc333902338be7c233c4eb1a24aea561c43
Thomas Freudenberg <info@thomasfreudenberg.com>
2012-11-07 13:44:00 +0100

    moved VS specific code to separate helper class

---
+++ Solutionizer/Helper/VisualStudioHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+using Solutionizer.Services;
+
+namespace Solutionizer.Helper {
+    public static class VisualStudioHelper {
+        public static VisualStudioVersion DetectVersion() {
+            using (var key = Registry.ClassesRoot.OpenSubKey("VisualStudio.DTE.11.0")) {
+                if (key != null) {
+                    return VisualStudioVersion.VS2012;
+                }
+            }
+            return VisualStudioVersion.VS2010;
+        }
+
+        public static string GetDefaultProjectsLocation(VisualStudioVersion visualStudioVersion) {
+            RegistryKey key = null;
+            string location = null;
+            try {
+                if (visualStudioVersion == VisualStudioVersion.VS2012) {
+                    key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\VisualStudio\11.0");
+                }
+                if (key == null) {
+                    key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\VisualStudio\10.0");
+                }
+                if (key != null) {
+                    location = key.GetValue("DefaultNewProjectLocation") as string;
+                }
+                if (String.IsNullOrEmpty(location)) {
+                    location = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                        "Visual Studio 2010",
+                        "Projects");
+                }
+                return location;
+            }
+            finally {
+                if (key != null) {
+                    key.Close();
+                }
+            }
+        }
+    }
+}
--- Solutionizer/Services/Settings.cs
+++ Solutionizer/Services/Settings.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.IO;
 using Caliburn.Micro;
-using Microsoft.Win32;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using Solutionizer.Helper;
 
 namespace Solutionizer.Services {
     public class Settings : PropertyChangedBase, ISettings {
@@ -20,44 +19,8 @@ namespace Solutionizer.Services {
         private string _rootPath;
 
         public Settings() {
-            _visualStudioVersion = DetectVisualStudioVersion();
-            _rootPath = GetDefaultProjectsLocation(_visualStudioVersion);
-        }
-
-        private static VisualStudioVersion DetectVisualStudioVersion() {
-            using (var key = Registry.ClassesRoot.OpenSubKey("VisualStudio.DTE.11.0")) {
-                if (key != null) {
-                    return VisualStudioVersion.VS2012;
-                }
-            }
-            return VisualStudioVersion.VS2010;
-        }
-        
-        private static string GetDefaultProjectsLocation(VisualStudioVersion visualStudioVersion) {
-            RegistryKey key = null;
-            string location = null;
-            try {
-                if (visualStudioVersion == VisualStudioVersion.VS2012) {
-                    key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\VisualStudio\11.0");
-                }
-                if (key == null) {
-                    key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\VisualStudio\10.0");
-                }
-                if (key != null) {
-                    location = key.GetValue("DefaultNewProjectLocation") as string;
-                }
-                if (String.IsNullOrEmpty(location)) {
-                    location = Path.Combine(
-                        Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
-                        "Visual Studio 2010",
-                        "Projects");
-                }
-                return location;
-            } finally {
-                if (key != null) {
-                    key.Close();
-                }
-            }
+            _visualStudioVersion = VisualStudioHelper.DetectVersion();
+            _rootPath = VisualStudioHelper.GetDefaultProjectsLocation(_visualStudioVersion);
         }
 
         public bool IsFlatMode {
