ecd19985d480450ea356025c3e144f8448a80518
darkiri <darkiri@gmail.com>
2012-12-04 23:26:47 +0100

    Filtering projects tree

--- Solutionizer/ViewModels/DirectoryViewModel.cs
+++ Solutionizer/ViewModels/DirectoryViewModel.cs
@@ -1,15 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Windows.Data;
 using Solutionizer.Models;
+using Solutionizer.Services;
 
 namespace Solutionizer.ViewModels {
     public class DirectoryViewModel : ItemViewModel {
+        private readonly ISettings _settings;
+
         private readonly ProjectFolder _projectFolder;
         private readonly List<DirectoryViewModel> _directories = new List<DirectoryViewModel>();
         private readonly List<ProjectViewModel> _projects = new List<ProjectViewModel>();
+        private readonly Func<ItemViewModel, bool> _filter;		
 
-        public DirectoryViewModel(DirectoryViewModel parent, ProjectFolder projectFolder) : base(parent) {
+        public DirectoryViewModel(ISettings settings, DirectoryViewModel parent, ProjectFolder projectFolder, Func<ItemViewModel, bool> filter) : base(parent) {
+            _settings = settings;
             _projectFolder = projectFolder;
+            _filter = filter;
         }
 
         public override string Name {
@@ -32,8 +41,18 @@ namespace Solutionizer.ViewModels {
             get { return _projects; }
         }
 
-        public IList<ItemViewModel> Children {
-            get { return _directories.Cast<ItemViewModel>().OrderBy(d => d.Name).Concat(_projects.OrderBy(p => p.Name)).ToList(); }
+        public ICollectionView Children {
+            get {
+                var children = _directories
+                    .Where(d => !_settings.SimplifyProjectTree || d.Children.Cast<ItemViewModel>().Any())
+                    .Cast<ItemViewModel>()
+                    .OrderBy(d => d.Name)
+                    .Concat(_projects.OrderBy(p => p.Name));
+
+                var childrenView = CollectionViewSource.GetDefaultView(children);
+                childrenView.Filter = item => !(item is ProjectViewModel) || _filter((ItemViewModel)item);
+                return childrenView;
+            }
         }
     }
 }
