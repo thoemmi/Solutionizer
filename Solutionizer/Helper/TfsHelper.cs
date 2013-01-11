9d96c70ee0f128ff0a9031e2e0e697fbcb12f1bf
Thomas Freudenberg <info@thomasfreudenberg.com>
2013-01-11 17:44:28 +0100

    runtime-binding of TFS assemblies

--- Solutionizer/Helper/TfsHelper.cs
+++ Solutionizer/Helper/TfsHelper.cs
@@ -1,20 +1,33 @@
 ﻿using System;
 using System.Globalization;
+using System.Reflection;
 using System.Windows.Forms;
-using Microsoft.TeamFoundation;
-using Microsoft.TeamFoundation.Client;
-using Microsoft.TeamFoundation.VersionControl.Client;
 
 namespace Solutionizer.Helper {
     public static class TfsHelper {
+        private static readonly Assembly _assemblyTfsClient;
+        private static readonly Assembly _assemblyTfsCommon;
+        private static readonly Assembly _assemblyTfsVcClient;
+
+        static TfsHelper() {
+            _assemblyTfsClient = Assembly.Load("Microsoft.TeamFoundation.Client, Version=10.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a, processorArchitecture=MSIL");
+            _assemblyTfsCommon = Assembly.Load("Microsoft.TeamFoundation.Common, Version=10.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a, processorArchitecture=MSIL");
+            _assemblyTfsVcClient = Assembly.Load("Microsoft.TeamFoundation.VersionControl.Client, Version=10.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a, processorArchitecture=MSIL");
+        }
+
         public static bool TryGetTeamProjectCollection(string localPath, ref Uri tfsName, out string tfsFolder) {
             tfsFolder = null;
+
+            if (_assemblyTfsClient == null || _assemblyTfsCommon == null || _assemblyTfsVcClient == null) {
+                return false;
+            }
+
             if (tfsName == null) {
-                var projectCollections = RegisteredTfsConnections.GetProjectCollections();
+                var projectCollections = GetProjectCollections();
                 if (projectCollections != null && projectCollections.Length == 1) {
                     tfsName = projectCollections[0].Uri;
                 } else {
-                    using (var dlg = new TeamProjectPicker(TeamProjectPickerMode.NoProject, false)) {
+                    using (var dlg = CreateTeamProjectPicker()) {
                         dlg.AcceptButtonText = "Select";
                         if (dlg.ShowDialog() != DialogResult.OK) {
                             return false;
@@ -33,18 +46,44 @@ namespace Solutionizer.Helper {
             return true;
         }
 
-        private static Workspace GetWorkspace(Uri tfsName, string localPath) {
-            var fallbackCredentialsProvider = (ICredentialsProvider) new UICredentialsProvider();
-            Workspace[] workspaceArray;
+        private static dynamic GetProjectCollections() {
+            var registeredTfsConnectionsType = _assemblyTfsClient.GetType("Microsoft.TeamFoundation.Client.RegisteredTfsConnections");
+            return registeredTfsConnectionsType.InvokeMember("GetProjectCollections", BindingFlags.Public | BindingFlags.Static | BindingFlags.InvokeMethod, null, null, null);
+        }
+
+        private static dynamic CreateTeamProjectPicker() {
+            var teamProjectPickerType = _assemblyTfsClient.GetType("Microsoft.TeamFoundation.Client.TeamProjectPicker");
+            var teamProjectPickerModeType = _assemblyTfsClient.GetType("Microsoft.TeamFoundation.Client.TeamProjectPickerMode");
+            var noProject = Enum.Parse(teamProjectPickerModeType, "NoProject");
+            return Activator.CreateInstance(teamProjectPickerType, new[] { noProject, false});
+        }
+
+        private static dynamic GetTeamProjectCollection(Uri tfsName, object credentialsProvider) {
+            var tfsTeamProjectCollectionFactoryType = _assemblyTfsClient.GetType("Microsoft.TeamFoundation.Client.TfsTeamProjectCollectionFactory");
+            return tfsTeamProjectCollectionFactoryType.InvokeMember("GetTeamProjectCollection",
+                                                                    BindingFlags.Public | BindingFlags.Static | BindingFlags.InvokeMethod,
+                                                                    null, null, new[] {
+                                                                        tfsName, credentialsProvider
+                                                                    });
+        }
+
+        private static dynamic GetWorkspace(Uri tfsName, string localPath) {
+            var versionControlServerType = _assemblyTfsVcClient.GetType("Microsoft.TeamFoundation.VersionControl.Client.VersionControlServer");
+            dynamic fallbackCredentialsProvider = Activator.CreateInstance(_assemblyTfsClient.GetType("Microsoft.TeamFoundation.Client.UICredentialsProvider"));
+            dynamic workspaceArray;
             try {
-                var projectCollection = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(tfsName, fallbackCredentialsProvider);
-                var versionControlServer = projectCollection.GetService<VersionControlServer>();
+                var projectCollection = GetTeamProjectCollection(tfsName, fallbackCredentialsProvider);
+                var versionControlServer = projectCollection.GetService(versionControlServerType);
                 workspaceArray = versionControlServer.QueryWorkspaces(null, versionControlServer.AuthorizedUser, Environment.MachineName);
-            } catch (TeamFoundationServerUnauthorizedException) {
-                fallbackCredentialsProvider.GetCredentials(tfsName, null);
-                var projectCollection = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(tfsName, fallbackCredentialsProvider);
-                var versionControlServer = projectCollection.GetService<VersionControlServer>();
-                workspaceArray = versionControlServer.QueryWorkspaces(null, versionControlServer.AuthorizedUser, Environment.MachineName);
+            } catch (Exception ex) {
+                if (ex.GetType().Name == "TeamFoundationServerUnauthorizedException") {
+                    fallbackCredentialsProvider.GetCredentials(tfsName, null);
+                    var projectCollection = GetTeamProjectCollection(tfsName, fallbackCredentialsProvider);
+                    var versionControlServer = projectCollection.GetService(versionControlServerType);
+                    workspaceArray = versionControlServer.QueryWorkspaces(null, versionControlServer.AuthorizedUser, Environment.MachineName);
+                } else {
+                    throw;
+                }
             }
 
             if (workspaceArray == null || workspaceArray.Length == 0) {
