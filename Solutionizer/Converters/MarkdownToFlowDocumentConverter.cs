052ad70b9d3a084ad81fe68e6b3ced9d8ab971bc
Thomas Freudenberg <info@thomasfreudenberg.com>
2013-10-03 17:39:08 +0200

    enable opening URLs

--- Solutionizer/Converters/MarkdownToFlowDocumentConverter.cs
+++ Solutionizer/Converters/MarkdownToFlowDocumentConverter.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
+using System.Linq;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Markup;
+using System.Windows.Navigation;
 using Solutionizer.Helper;
 
 namespace Solutionizer.Converters {
@@ -17,11 +22,34 @@ namespace Solutionizer.Converters {
 
             text = _markdown.Transform(text);
             text = HtmlToXamlConverter.ConvertHtmlToXaml(text, true);
-            return XamlReader.Parse(text);
+            var flowDocument = (FlowDocument)XamlReader.Parse(text);
+            SubscribeToAllHyperlinks(flowDocument);
+            return flowDocument;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
             return DependencyProperty.UnsetValue;
+        }
+
+        private void SubscribeToAllHyperlinks(FlowDocument flowDocument) {
+            var hyperlinks = GetVisuals(flowDocument).OfType<Hyperlink>();
+            foreach (var link in hyperlinks) {
+                link.RequestNavigate += OnRequestNavigate;
+            }
+        }
+
+        private static IEnumerable<DependencyObject> GetVisuals(DependencyObject root) {
+            foreach (var child in LogicalTreeHelper.GetChildren(root).OfType<DependencyObject>()) {
+                yield return child;
+                foreach (var descendants in GetVisuals(child)) {
+                    yield return descendants;
+                }
+            }
+        }
+
+        private static void OnRequestNavigate(object sender, RequestNavigateEventArgs e) {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
---
+++ Solutionizer/Infrastructure/HyperlinkNavigateBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Windows.Documents;
+using System.Windows.Interactivity;
+using System.Windows.Navigation;
+
+namespace Solutionizer.Infrastructure {
+    public class HyperlinkNavigateBehavior : Behavior<Hyperlink> {
+        protected override void OnAttached() {
+            base.OnAttached();
+            AssociatedObject.RequestNavigate += AssociatedObjectRequestNavigate;
+        }
+
+        protected override void OnDetaching() {
+            AssociatedObject.RequestNavigate -= AssociatedObjectRequestNavigate;
+            base.OnDetaching();
+        }
+
+        private void AssociatedObjectRequestNavigate(object sender, RequestNavigateEventArgs e) {
+            var uri = AssociatedObject.NavigateUri;
+
+            if (uri != null) {
+                Process.Start(new ProcessStartInfo(uri.ToString()));
+            }
+        }
+    }
+}
